04658ccda70335c563caf416eb96e736bcc73ee2
Jean-Baptiste Tricheur <jean-baptiste.tricheur@hotmail.fr>
2020-03-19 19:39:40 +0100

    validations rules and migrations done

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,4 +4,16 @@ class Project < ApplicationRecord
   has_many :attendances, foreign_key: 'involved_project_id'
   has_many :participants, class_name: 'User', through: :attendances, foreign_key: 'participant_id'
 
+  validates :title, presence: true, length: { in: 5..140 }
+  validates :location, presence: true
+  validates :description, presence: true, length: { in: 20..1000 }
+  validates :number_of_participants, 
+    presence: true,
+    numericality: {only_integer:true},
+    numericality: { greater_than_or_equal_to: 0}
+  validates :amount_needed,
+    presence: true,
+    numericality: {only_integer:true},
+    numericality: { greater_than_or_equal_to: 0}
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,12 @@ class User < ApplicationRecord
   has_many :attendances, foreign_key: 'participant_id'
   has_many :involved_projects, class_name: 'Project', through: :attendances, foreign_key: 'involved_project_id'
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :description, 
+    presence: true,
+    length: {minimum: 60}
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
