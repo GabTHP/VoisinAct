b330d0ae030bb061635ff6893eb3be8e5b95992f
Jean-Baptiste Tricheur <jean-baptiste.tricheur@hotmail.fr>
2020-03-19 15:29:43 +0100

    model likes et modifications

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,5 +3,5 @@ class Project < ApplicationRecord
   belongs_to :architect, class_name: 'User', foreign_key: 'architect_id'
   has_many :attendances, foreign_key: 'involved_project_id'
   has_many :participants, class_name: 'User', through: :attendances, foreign_key: 'participant_id'
-
+  has_many :likes
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :organised_projects, class_name: 'Project'
   has_many :attendances, foreign_key: 'participant_id'
   has_many :involved_projects, class_name: 'Project', through: :attendances, foreign_key: 'involved_project_id'
-
+  has_many :likes
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
