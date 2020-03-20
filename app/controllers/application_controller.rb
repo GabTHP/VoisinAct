baf3239882bcfe9cb2fdf7c09a18eaeda987b60f
GabTHP <gabriel.bouakira@hotmail.fr>
2020-03-20 12:32:01 +0100

    ajout des attributs user pour edit, sign in et mon profil

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,16 @@ def set_layout
       'application'
     end
  end
- protect_from_forgery with: :null_session
+    protect_from_forgery with: :exception
+
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+
+        def configure_permitted_parameters
+            devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :description, :email, :password) }
+            devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:first_name, :last_name, :description, :email, :password, :current_password) }
+        end
 
  # Prevent CSRF attacks by raising an exception.
     # For APIs, you may want to use :null_session instead.
