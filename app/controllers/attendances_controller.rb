df9cf658c755b866e77260268a779047035cdfd9
Nathan Baliteau <nathanbaliteau@gmail.com>
2020-03-22 10:21:42 +0100

    attendances to development

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -2,9 +2,6 @@ class AttendancesController < ApplicationController
   
   def create
     @attendance = Attendance.new(involved_project_id: params[:project_id], participant: current_user)
-    puts "$"*60
-    puts params
-    puts "$"*60
     if @attendance.save
       render project_path(params[:project_id])
     else
