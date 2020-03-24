460ba95536af4c247ca0d4c208e9e14b8756f758
Thibault Peres <peresthibaul@gmail.com>
2020-03-24 10:49:16 +0100

    ajout page contact

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
 
   root 'projects#index'
   resources :projects
+  get '/contact/', to: 'pages#contact'
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
