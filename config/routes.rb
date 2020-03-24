b18d84f33cfe0bb5b5d53dee88687e5ed23b261b
Claire Dubas <dub.claire@icloud.com>
2020-03-24 11:29:59 +0100

    lien du like

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :users
   resources :cities
   resources :attendances, only: [:create, :destroy]
-  resources :likes
+  resources :likes, only:[:show]
 
   root 'projects#index'
   resources :projects
